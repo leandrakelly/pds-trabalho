8dab6370e5d3fd95384d6343f73098208639ad07
unknown <leandra.lima.ext@dasa.com.br>
2022-06-14 13:36:13 -0300

    config dotenv

---
+++ src/database/data-source.ts
@@ -0,0 +1,23 @@
+import "reflect-metadata"
+import "dotenv/config"
+import { DataSource } from "typeorm"
+
+const AppDataSource = new DataSource({
+    type: process.env.DATABASE_TYPE as any,
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT),
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    entities: [__dirname + '/../entities/*.ts']
+});
+
+AppDataSource.initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!")
+    })
+    .catch((err) => {
+        console.error("Error during Data Source initialization", err)
+    });
+
+export { AppDataSource };
