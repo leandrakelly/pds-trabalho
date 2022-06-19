673a22b05379ecad4eff365aed89f52b84c2bb8e
leandrakelly <leandrakell@gmail.com>
2022-06-19 17:11:31 -0300

    add delete route into routes

--- src/routes/courses.routes.ts
+++ src/routes/courses.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { createCourseController } from "../modules/createCourse/CreateCourseFactory";
+import { deleteCourseController } from "../modules/deleteCourse/DeleteCourseFactory";
 import { findCourseByIdController } from "../modules/findCourseById/FindCourseByIdFactory";
 import { listCoursesController } from "../modules/listCourses/ListCoursesFactory";
 
@@ -15,6 +16,10 @@ coursesRouter.get('/', (request, response) => {
 
 coursesRouter.get('/:id', (request, response) => {
     return findCourseByIdController.handle(request, response);
+})
+
+coursesRouter.delete('/:id', (request, response) => {
+    return deleteCourseController.handle(request, response);
 })
 
 export { coursesRouter };
