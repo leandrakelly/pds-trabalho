44463fc7603625259048efe58db1d3f32ea83cb0
Gabriel Correia <gabrielcorreiads@outlook.com>
2022-06-19 17:17:24 -0300

    add edit course route into routes

--- src/routes/courses.routes.ts
+++ src/routes/courses.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { createCourseController } from "../modules/createCourse/CreateCourseFactory";
 import { deleteCourseController } from "../modules/deleteCourse/DeleteCourseFactory";
+import { editCourseController } from "../modules/editCourse/EditCourseFactory";
 import { findCourseByIdController } from "../modules/findCourseById/FindCourseByIdFactory";
 import { listCoursesController } from "../modules/listCourses/ListCoursesFactory";
 
@@ -16,6 +17,10 @@ coursesRouter.get('/', (request, response) => {
 
 coursesRouter.get('/:id', (request, response) => {
     return findCourseByIdController.handle(request, response);
+})
+
+coursesRouter.put('/:id', (request, response) => {
+    return editCourseController.handle(request, response);
 })
 
 coursesRouter.delete('/:id', (request, response) => {
