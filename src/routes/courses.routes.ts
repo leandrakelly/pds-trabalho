89370cea64dfb55c43ae246e2703782926d2f2e9
Mônica Yasmin <monicayasmin@alu.ufc.br>
2022-06-18 21:24:06 -0300

    add route for list courses

--- src/routes/courses.routes.ts
+++ src/routes/courses.routes.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import { createCourseController } from "../modules/createCourse/CreateCourseFactory";
+import { listCoursesController } from "../modules/listCourses/ListCoursesFactory";
 
 const coursesRouter = Router();
 
 coursesRouter.post('/', (request, response) => {
     return createCourseController.handle(request, response);
 });
+
+coursesRouter.get('/', (request, response) => {
+    return listCoursesController.handle(request, response);
+})
+
 
 export { coursesRouter };
