e247b10158cefd65d5864dc6bd9bb53f87083bbd
Mônica Yasmin <monicayasmin@alu.ufc.br>
2022-06-18 21:11:41 -0300

    add bussiness rules for create course service

--- src/modules/createCourse/CreateCourseService.ts
+++ src/modules/createCourse/CreateCourseService.ts
@@ -5,6 +5,19 @@ export class CreateCourseService {
     constructor(private coursesRepository: ICoursesRepository) {}
 
     async execute(data: ICreateCourseDTO) {
-        return await this.coursesRepository.save(data);
+        let timePattern: string = '^(2[0-3]|[01]?[0-9]):([0-5]?[0-9])$';
+
+        if (data.start_time.match(timePattern) && data.end_time.match(timePattern)){
+            let start_time_hour: number = parseInt(data.start_time.split(':')[0]);
+            let end_time_hour: number = parseInt(data.end_time.split(':')[0]);
+            
+            if (start_time_hour < end_time_hour) {
+                return await this.coursesRepository.save(data);
+            } else {
+                throw new Error("The end time should be past the start time");
+            }
+        } else {
+            throw new Error("The time format should be hh:mm");
+        }
     }
 }
