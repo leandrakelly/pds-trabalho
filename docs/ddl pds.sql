8239b06beab18cd99b8d1043c74f09f53ae246e6
Mônica Yasmin <monicayasmin@alu.ufc.br>
2022-07-02 15:54:13 -0300

    change ddl file and db diagram

---
+++ docs/ddl pds.sql
@@ -0,0 +1,67 @@
+CREATE TYPE "users_role" AS ENUM (
+  'student',
+  'admin'
+);
+
+CREATE TYPE "courses_status" AS ENUM (
+  'new',
+  'on_going',
+  'completed',
+  'canceled'
+);
+
+CREATE TABLE "users" (
+  "id" uuid PRIMARY KEY,
+  "email" varchar,
+  "password" varchar,
+  "role" users_role
+);
+
+CREATE TABLE "courses_users" (
+  "course_id" uuid,
+  "user_id" uuid,
+  "created_at" timestamp,
+  PRIMARY KEY ("course_id", "user_id")
+);
+
+CREATE TABLE "courses" (
+  "id" uuid PRIMARY KEY,
+  "name" varchar,
+  "start_time" time,
+  "end_time" time,
+  "status" courses_status
+);
+
+CREATE TABLE "courses_teachers" (
+  "course_id" uuid,
+  "teacher_id" uuid,
+  PRIMARY KEY ("course_id", "teacher_id")
+);
+
+CREATE TABLE "teachers" (
+  "id" uuid PRIMARY KEY,
+  "name" varchar
+);
+
+CREATE TABLE "courses_classrooms" (
+  "course_id" uuid,
+  "classroom_id" uuid,
+  PRIMARY KEY ("course_id", "classroom_id")
+);
+
+CREATE TABLE "classrooms" (
+  "id" uuid PRIMARY KEY,
+  "number" varchar
+);
+
+ALTER TABLE "courses_classrooms" ADD FOREIGN KEY ("course_id") REFERENCES "courses" ("id");
+
+ALTER TABLE "courses_classrooms" ADD FOREIGN KEY ("classroom_id") REFERENCES "classrooms" ("id");
+
+ALTER TABLE "courses_teachers" ADD FOREIGN KEY ("course_id") REFERENCES "courses" ("id");
+
+ALTER TABLE "courses_teachers" ADD FOREIGN KEY ("teacher_id") REFERENCES "teachers" ("id");
+
+ALTER TABLE "courses_users" ADD FOREIGN KEY ("course_id") REFERENCES "courses" ("id");
+
+ALTER TABLE "courses_users" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
